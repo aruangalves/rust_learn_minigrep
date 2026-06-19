9fefd8d7ca6364ed563f961dc24b9d1c82ead94b
Aruan Galves <aruangalves@gmail.com>
2026-06-19 10:55:18 -0300

    Saving the argument values in variables

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,13 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    //&args[0] returns the program name
+    //e.g. running through cargo run returns
+    //     the value "target/debug/minigrep"
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
