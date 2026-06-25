faff3920966775cbe905bdb6ced9a450e39259ae
Aruan Galves <aruangalves@gmail.com>
2026-06-25 15:07:38 -0300

    Added test for the search function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::error::Error;
 use std::fs;
 use std::process;
 
+use minigrep::search;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,8 +13,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    //println!("Searching for {}", config.query);
+    //println!("In file {}", config.file_path);
 
     if let Err(err) = run(config) {
         println!("Application error: {}", err);
@@ -23,7 +25,11 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{}", contents);
+    //println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
