44b59f87db4f731b45e059a5fa96c587b91fab30
Aruan Galves <aruangalves@gmail.com>
2026-06-25 14:35:00 -0300

    Added error handling for few arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -21,7 +25,10 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         //&args[0] returns the program name
         //e.g. running through cargo run returns
         //     the value "target/debug/minigrep"
@@ -30,6 +37,6 @@ impl Config {
         //let file_path = &args[2];
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
